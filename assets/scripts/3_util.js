2d5c2b0d4786ae41f7408a7736141b634b3667cb
DefaltSimon <simon.peter.goricar@gmail.com>
2018-07-04 14:53:46 +0200

    Registration now fully works
    Add messages for successful and failed registration

---
+++ assets/scripts/3_util.js
@@ -0,0 +1,10 @@
+// UTILITIES
+
+const StatusTypes = {
+    OK: "ok",
+    INVALID_ARGUMENT: "invalid_argument",
+    WRONG_LOGIN_INFO: "wrong_login_info",
+
+    USER_ALREADY_EXISTS: "user_already_exists",
+    EMAIL_ALREADY_REGISTERED: "email_registered"
+};
