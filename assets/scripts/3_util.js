22ec592e4b0239e8102239ffa929d36b5c1a3bda
DefaltSimon <simon.peter.goricar@gmail.com>
2018-07-05 20:39:28 +0200

    Add ledinaAjax, add Object.assign polyfill

--- assets/scripts/3_util.js
+++ assets/scripts/3_util.js
@@ -8,3 +8,35 @@ const StatusTypes = {
     USER_ALREADY_EXISTS: "user_already_exists",
     EMAIL_ALREADY_REGISTERED: "email_registered"
 };
+
+
+// AJAX helper
+
+function ledinaAjax(type, url, data, callback, kwargs) {
+    if (typeof type === "undefined") {
+        type = "POST"
+    }
+    if (typeof url === "undefined") {
+        throw "Missing url"
+    }
+
+    var ajaxOptions = {
+        type: type,
+        url: url,
+        contentType: "application/json",
+        async: "false",
+        data: data,
+        // disable json parsing
+        dataType: "text",
+
+        // attach callback function
+        complete: callback
+    };
+
+    // this applies additional options
+    if (typeof kwargs !== "undefined") {
+        ajaxOptions = Object.assign({}, ajaxOptions, kwargs)
+    }
+
+    $.ajax(ajaxOptions)
+}
